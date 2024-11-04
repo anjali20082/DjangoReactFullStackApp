57bea48e45729cb686da0ff2a7544899b2895227
anjali20082 <jainanjali13224@gmail.com>
2024-11-04 15:31:49 -0500

    add backend url again

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
 // const apiUrl = "/choreo-apis/awbo/backend/rest-api-be2/v1.0";
-const apiUrl = "https://ed53fb5d-4e04-40ce-94fe-48f723a11316-dev.e1-us-east-azure.choreoapis.dev/djangoreactapp/backend/v1";
+const apiUrl = "/choreo-apis/djangoreactapp/backend/v1";
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
 });
